5e308518d33bf98718c67eefde8626b00de1518d
bogdanleustean <bogdan.leustean@yahoo.com>
2021-05-08 23:00:18 +0300

    thirdCommitOfUdemyJavaTutorial

--- src/section6/clases/Car.java
+++ src/section6/clases/Car.java
@@ -1,5 +1,23 @@
 package section6.clases;
 
 public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
 
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if(validModel.equals("carrera")||validModel.equals("commodpre")) {
+            this.model = model;
+        }
+        else {
+            this.model = "unknown";
+        }
+    }
 }
--- src/section6/clases/Main.java
+++ src/section6/clases/Main.java
@@ -3,5 +3,9 @@ package section6.clases;
 public class Main {
     public static void main(String[] args) {
 // test for git
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+        System.out.println("model is " +porsche.getModel());
     }
 }

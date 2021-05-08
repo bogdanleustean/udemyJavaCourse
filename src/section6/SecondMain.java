2fc0052156b06699a05d2dffa7e2cf0aa4c3f817
bogdanleustean <bogdan.leustean@yahoo.com>
2021-05-08 23:36:52 +0300

    thirdCommitOfUdemyJavaTutorial

--- src/section6/SecondMain.java
+++
@@ -1,4 +0,0 @@
-package section6;
-
-public class SecondMain {
-}
--- src/section6/clases/Main.java
+++ src/section6/clases/Main.java
@@ -5,7 +5,21 @@ public class Main {
 // test for git
         Car porsche = new Car();
         Car holden = new Car();
-        porsche.setModel("Carrera");
-        System.out.println("model is " +porsche.getModel());
+        porsche.setModel("911");
+      //  System.out.println("model is " +porsche.getModel());
+
+        SumCalculatorChallenge calculator = new SumCalculatorChallenge();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4.0);
+
+        System.out.println("add = " + calculator.getAdditionalResult());
+        System.out.println("subtract = " + calculator.getSubtractionResult());
+
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+
+        System.out.println("multiply = "+calculator.getMultiplicationResult());
+        System.out.println("divide = "+calculator.getDivisionResult());
+
     }
 }
